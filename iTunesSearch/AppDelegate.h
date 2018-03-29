3fe204f9538df786744f1c558b8ca712fc71d364
HDZTony <954504788@qq.com>
2018-03-29 23:16:42 +0800

    Initial Commit

---
+++ iTunesSearch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iTunesSearch
+//
+//  Created by 何东洲 on 2018/3/29.
+//  Copyright © 2018年 何东洲. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iTunesSearch/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iTunesSearch
+//
+//  Created by 何东洲 on 2018/3/29.
+//  Copyright © 2018年 何东洲. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
